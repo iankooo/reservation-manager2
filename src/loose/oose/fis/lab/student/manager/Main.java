ee05809a6d825bb759f8adf3ae5c2ddd505c75ac
Schipala Ianko <ianko_radoslav@yahoo.com>
2019-04-09 21:25:38 +0300

    Added documentation to the most important java classes

--- src/loose/oose/fis/lab/student/manager/Main.java
+++ src/loose/oose/fis/lab/student/manager/Main.java
@@ -5,8 +5,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+/**
+* This is the main class of the reservation manager
+* Currently this class contains some dummy users and some dummy reservations.
+* Finally, after creating them, the main method of this class prints them out.
+*
+* Feel free to run the main method at any time. :)
+*/
 
 public class Main extends Application {
+    /**
+* @param args not used
+*
+* this method starts the application :)
+*/
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -16,8 +28,12 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-
+  //creating the users
     public static void main(String[] args) {
         launch(args);
     }
+  //creating reservations
+  //adding all the reservations to a list
+  //printing out the reservations
+
 }
